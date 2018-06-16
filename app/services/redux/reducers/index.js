edbda465b39ff22e4c4299b1a9d7a10007acad85
Michael Mitrakos <mitrakmt@Michaels-MacBook-Pro.local>
2018-06-16 14:18:22 -0700

    feat: leaderboard progress

--- app/services/redux/reducers/index.js
+++ app/services/redux/reducers/index.js
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux'
 import authReducer from './auth'
 import gameResultsReducer from './gameResults'
+import leaderboardReducer from './leaderboard/leaderboard'
 
 const rootReducer = combineReducers({
   auth: authReducer,
