61672c265b96aa3abe330132c3b98b33cd0cd913
Michael Flores <mike@michaelflores.io>
2018-06-19 18:12:08 -0700

    fix: Clear redux state for results on GAME_START

--- app/services/redux/reducers/gameresults/index.js
+++ app/services/redux/reducers/gameresults/index.js
@@ -1,4 +1,5 @@
 import { GAME_RESULTS } from '../../actions/gameresults'
+import { GAME_START } from '../../actions/gameplay'
 
 const initialState = {
   remainingTime: null,
@@ -15,6 +16,8 @@ const gameResultsReducer = (state = initialState, action) => {
   switch (action.type) {
     case GAME_RESULTS:
       return { ...state, ...action.payload }
+    case GAME_START:
+      return { ...state, ...initialState }
     default:
       return state
   }
