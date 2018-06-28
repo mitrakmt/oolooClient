2467e7dbaa0bad69397ac9c7a8b876dcc5f6a4fd
jaimemendozadev <jaimemendozadev@gmail.com>
2018-06-28 10:08:31 -0700

    Create working version of slot machine
    Add NPM package to add animated slot
    machine for Match Found view.
    Icon library is missing some of the
    medical icons, limiting icon display
    options.

--- app/components/match-found/index.js
+++ app/components/match-found/index.js
@@ -4,10 +4,17 @@ import { Text, View, Image } from 'react-native'
 // import { connect } from 'react-redux'
 // import PropTypes from 'prop-types'
 
+import SlotMachine from 'react-native-slot-machine'
+import Icon from 'react-native-vector-icons/FontAwesome'
+
 // import tracker from '../../services/analytics-tracker/analyticsTracker'
 import styles from './styles'
 
 // import socketMiddleware from '../../services/socket-io-client'
+
+const MedicalIcons = ['stethoscope', 'heartbeat', 'ambulance', 'flask']
+
+// dna, tablets, hospital, file-prescription, notes-medical
 
 class MatchFound extends Component {
   constructor(props) {
@@ -20,6 +27,15 @@ class MatchFound extends Component {
   }
 
   componentDidMount = () => {}
+
+  renderSlotIcon = index => {
+    const displayIndex = index > 3 ? index % 2 : index
+    return (
+      <Text>
+        <Icon color="#fff" name={MedicalIcons[displayIndex]} size={25} />
+      </Text>
+    )
+  }
 
   render() {
     return (
@@ -53,7 +69,13 @@ class MatchFound extends Component {
               />
             </View>
 
-            <View style={styles.buttonContainerStyle} />
+            <View style={styles.buttonContainerStyle}>
+              <SlotMachine
+                text="0123"
+                range="0123456789"
+                renderContent={c => this.renderSlotIcon(c)}
+              />
+            </View>
           </View>
         </View>
       </View>
